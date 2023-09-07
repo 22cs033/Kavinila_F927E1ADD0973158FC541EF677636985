2900cce40e279df2dfa92efee426e6d2363804ee
22cs033 <144302155+22cs033@users.noreply.github.com>
2023-09-07 09:04:45 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+#Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
+
+#Online Python computer interpreters to Fun Python online.
+#write Python 3 code in this online editor and run it.
+year = 2036
+if(year%4 ==0):
+   print("leap year")
+else:
+    print("Not leap year")
